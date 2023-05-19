7cc4cf422bf71c11b32dec994f3da4cd94461fe7
sztom <sztom@TOMI0928>
2023-05-19 10:37:36 +0200

    Add project files.

---
+++ ConsoleApp7/Pilota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+   internal class Pilota
+   {
+        public string name { get; set; }
+        public string szuletes { get; set; }
+        public string nemzet { get; set; }
+        public int rszam { get; set; }
+        public int year { get; set; }
+
+        public Pilota(string sor)
+        {
+            var adat = sor.Split(';');
+            name = adat[0];
+            szuletes = adat[1];
+            nemzet = adat[2];
+            rszam = (adat[3].Length > 0) ? int.Parse(adat[3]) : 0;
+            year = int.Parse(adat[1].Substring(0, 4));
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;                     
+using System.IO;                 
+using System.Text;                
+using System.Collections.Generic; 
+using System.Linq;               
+using ConsoleApp7;
+using System.Collections;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Beolvas();
+        darab();
+        utolsonev();
+        szazad();
+    }
+
+
+
+
+
+    static List<Pilota> lista = new List<Pilota>();
+    public static void Beolvas()
+    {
+
+        var f = new StreamReader("pilotak.csv", Encoding.UTF8);
+        string elso = f.ReadLine(); //lehagyja az elso sort
+
+        while (!f.EndOfStream)
+        {
+            lista.Add(new Pilota(f.ReadLine()));
+        }
+        f.Close();
+    }
+
+
+
+
+    public static void darab()
+    {
+        Console.WriteLine($"3f: {lista.Count}");
+
+    }
+
+
+
+    public static void szazad() {
+        var ev = ( //Kiválasztja a 1901 előtt született neveket
+        from sor in lista
+        where sor.year < 1901
+        select (sor.name, sor.szuletes)
+        );
+        Console.WriteLine($"5f: ");
+        foreach (var sor in ev)
+        {
+            Console.WriteLine($"{sor.name}, [{sor.szuletes}] ");
+        }
+    }
+    public static void utolsonev()
+    {
+        string lname = lista.Last().name;
+        Console.WriteLine($"4f: {lname}");
+
+    }
+
+
+
+
+}
